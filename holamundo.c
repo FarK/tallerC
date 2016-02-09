d503e1f6be772d3160a3daa18d28cd186d20ebc2
Carlos Falgueras García <carlosfg@riseup.net>
2016-02-09 16:31:24 +0100

    Funciones III
    Optimizando funciones, parámetros por referencia, punteros a estructuras.

--- holamundo.c
+++ holamundo.c
@@ -15,51 +15,47 @@ struct datos {
 	int telefono;
 };
 
-struct datos pregunta_datos();
-void imprime_datos(struct datos persona);
+void pregunta_datos(struct datos *persona);
+void imprime_datos(struct datos *persona);
 
 // Entrada al programa
 int main() {
 	struct datos persona1, persona2;
 
 	printf("DATOS PERSONA 1\n");
-	persona1 = pregunta_datos();
+	pregunta_datos(&persona1);
 
 	printf("DATOS PERSONA 2\n");
-	persona2 = pregunta_datos();
+	pregunta_datos(&persona2);
 
-	imprime_datos(persona1);
-	imprime_datos(persona2);
+	imprime_datos(&persona1);
+	imprime_datos(&persona2);
 }
 
-struct datos pregunta_datos()
+void pregunta_datos(struct datos *persona)
 {
-	struct datos persona;
-
 	/* Ojo con scanf! */
 	printf("> ¿Cómo te llamas?\n");
-	scanf("%256s", persona.nombre);
+	scanf("%256s", persona->nombre);
 
 	printf("> ¿Cuál es tu primer apellido?\n");
-	scanf("%256s", persona.apellido1);
+	scanf("%256s", persona->apellido1);
 
 	printf("> ¿Cuál es tu segundo apellido?\n");
-	scanf("%256s", persona.apellido2);
+	scanf("%256s", persona->apellido2);
 
 	printf("> ¿Cuál es tu DNI?\n");
-	scanf("%10s", persona.dni);
+	scanf("%10s", persona->dni);
 
 	printf("> ¿Cuál es tu teléfono?\n");
-	scanf("%9d", &persona.telefono);
-
-	return persona;
+	scanf("%9d", &persona->telefono);
 }
 
-void imprime_datos(struct datos persona)
+void imprime_datos(struct datos *persona)
 {
 	printf("> Hola %s %s %s!"
 	       "\n\tDNI  = %s"
 	       "\n\ttlfn = %d\n",
-	       persona.nombre, persona.apellido1, persona.apellido2,
-	       persona.dni, persona.telefono);
+	       persona->nombre, persona->apellido1, persona->apellido2,
+	       persona->dni, persona->telefono);
 }
