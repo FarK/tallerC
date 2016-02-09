1f335cad693d12793cbc2425f704345976f90689
Carlos Falgueras García <carlosfg@riseup.net>
2016-02-09 07:01:31 +0100

    Hola Mundo
    Includes, printf y compilación básica.
    
    Compílame y ejecútame así:
    	$> gcc holamundo.c -o holamundo
    	$> ./holamundo
    	Hola mundo!

---
+++ holamundo.c
@@ -0,0 +1,12 @@
+/*
+ * Los includes de la librería estándar nos proveen de funciones básicas
+ */
+// STanDandard Input-Output: funciones para el manejo de entrada-salida
+#include <stdio.h>
+
+// Entrada al programa
+int main() {
+	printf("Hola mundo!\n");  // PRINTs Formatted: para escribir en consola
+
+	return 0;
+}
