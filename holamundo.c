517cdad912a971e7b38c8525500281d688204c9d
Carlos Falgueras García <carlosfg@riseup.net>
2016-02-09 09:40:28 +0100

    Arrays
    Arrays y cadenas

--- holamundo.c
+++ holamundo.c
@@ -6,32 +6,39 @@
 
 // Entrada al programa
 int main() {
-	// Algunas variables de distintos tipos
-	char caracter = 'c';  // Declaración e inicialización
-	int numero_entero;    // Declaración de una variable sin inicializar
-	float numero_real;    // Variable en coma flotante
+	int array[4] = {11, 22, 33, 44};
 
-	long int numero_entero_grande;
-	unsigned char byte_sin_signo;
-	double numero_real_preciso;
+	array[2] = 0;
 
-	numero_real = 3.3;
-	numero_entero = numero_real * 2;
+	int i;
+	printf("array = {\n");
+	for (i = 0; i < 4; i++)
+		printf("\t[%d] = %d\n", i, array[i]);
+	printf("}\n");
 
-	// Impresión de variables
-	printf("VALORES:\n");
-	printf("\tcaracter      = %c\n", caracter);
-	printf("\tnumero_entero = %d\n", numero_entero);
-	printf("\tnumero_real   = %f\n", numero_real);
 
-	// Tamaños de cada tipo
-	printf("TAMAÑOS:\n");
-	printf("\tchar          = %lu\n", sizeof(caracter));
-	printf("\tunsigned char = %lu\n", sizeof(byte_sin_signo));
-	printf("\tint           = %lu\n", sizeof(int));
-	printf("\tlong int      = %lu\n", sizeof(numero_entero_grande));
-	printf("\tfloat         = %lu\n", sizeof(float));
-	printf("\tdouble        = %lu\n", sizeof(numero_real_preciso));
+	/***********
+	 * CADENAS *
+	 ***********/
+	// Declaración con tamaño explicito e inicialización de cada elemento
+	char hola[5] = {'h', 'o', 'l', 'a', '\0'};
+	printf("\nhola = {\n");
+	for (i = 0; i < 4; i++)
+		printf("\t[%d] = %c\n", i, hola[i]);
+	printf("}\n");
+
+	printf("\nhola = \"%s\"\n", hola);
+
+
+	// Declaración con tamaño implícito e inicialización como cadena
+	char mundo[] = "mundo";
+
+	printf("\nmundo = {\n");
+	for (i = 0; i < 5; i++)
+		printf("\t[%d] = %c\n", i, mundo[i]);
+	printf("}\n");
+
+	printf("\nmundo = \"%s\"\n", mundo);
 
 	return 0;
 }
