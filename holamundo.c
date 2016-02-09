bda87790ff5d9ad49490555c63c2d8ab7d945f1e
Carlos Falgueras García <carlosfg@riseup.net>
2016-02-09 16:11:34 +0100

    Arrays multidimensionales

--- holamundo.c
+++ holamundo.c
@@ -9,29 +9,26 @@
 
 // Entrada al programa
 int main() {
-	char nombre[LEN];
-	char apellido1[LEN];
-	char apellido2[LEN];
-	char dni[10];
-	int telefono;
+	char datos[5][LEN];
 
 	/* Ojo con scanf! */
 	printf("> ¿Cómo te llamas?\n");
-	scanf("%256s", nombre);
+	scanf("%256s", datos[0]);
 
 	printf("> ¿Cuál es tu primer apellido?\n");
-	scanf("%256s", apellido1);
+	scanf("%256s", datos[1]);
 
 	printf("> ¿Cuál es tu segundo apellido?\n");
-	scanf("%256s", apellido2);
+	scanf("%256s", datos[2]);
 
 	printf("> ¿Cuál es tu DNI?\n");
-	scanf("%10s", dni);
+	scanf("%10s", datos[3]);
 
 	printf("> ¿Cuál es tu teléfono?\n");
-	scanf("%9d", &telefono);
+	scanf("%9s", datos[4]);
 
 	printf("> Hola %s %s %s!"
 	       "\n\tDNI  = %s"
-	       "\n\ttlfn = %d\n", nombre, apellido1, apellido2, dni, telefono);
+	       "\n\ttlfn = %s\n",
+	       datos[0], datos[1], datos[2], datos[3], datos[4]);
 }
